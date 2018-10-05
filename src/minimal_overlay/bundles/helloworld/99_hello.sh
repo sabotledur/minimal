a48c813ccba0a9c38efbc6f059862ff2a9dd18d5
Alex Le Saux <alesaux@wan-pulse.com>
2018-10-05 17:20:58 +0200

    rename variables

--- src/minimal_overlay/bundles/helloworld/99_hello.sh
+++ src/minimal_overlay/bundles/helloworld/99_hello.sh
@@ -4,32 +4,43 @@ set -e
 
 echo -e '##########################--------------___________________________ \e[34mWanpulse Imaging Tool\e[0m ___________________________--------------##########################'
 
+
+#extract informations from config file 
+#Mount hidden partition
+
+#Read config file
+# Read the 'FORCE_32_BIT_BINARIES' property from '.config'
+#FORCE_32_BIT_BINARIES="$(grep -i ^FORCE_32_BIT_BINARIES $SRC_DIR/.config | cut -f2 -d'=')"
+
+#backup folder on FTP server
+BACKUP_PATH='chip-pc'
+
 #backup files name
-fileName='-back.img.tar.gz'
+BACKUP_FILENAME='-back.img.tar.gz'
 
 #size of each packet captured
-bs='50M'
+BLOCK_SIZE='50M'
 
 #number of packet to capture
-count=153
+TOTAL_PACKETS_NUMBER=153
 
 #ftp server address
-ftpServer='192.168.2.159'
+FTP_SERVER='192.168.2.159'
 
 #ftp server port
-ftpServerPort='21'
+FTP_SERVER_PORT='21'
 
 #ftp user
-ftpUser='alex'
+FTP_USERNAME='alex'
 
 #ftp password
-ftpPwd='alex123456'
+FTP_PASSWORD='alex123456'
 
 #disk to backup
-ddToBackup='/dev/sda'
+DISK_TARGET='/dev/sda'
 
 #partition type
-partitionType='ntfs'
+PARTITION_TYPE='ntfs'
 
 
 
@@ -40,71 +51,71 @@ if [ mode = "B" ]
 then
 
 	echo "Configuration of disk backup:"
-	echo 'Backup files name:' $fileName
-	echo 'Block size:' $bs
-	echo 'Number of block to capture:' $count
-	echo 'FTP server address:' $ftpServer
-	echo 'FTP server port:' $ftpServerPort
-	echo 'FTP server user:' $ftpUser
-	echo 'FTP server password:' $ftpPwd
-	echo 'Disk to backup:' $ddToBackup
-	echo 'Partition type:' $partitionType	
+	echo 'Backup files name:' $BACKUP_FILENAME
+	echo 'Block size:' $BLOCK_SIZE
+	echo 'Number of block to capture:' $TOTAL_PACKETS_NUMBER
+	echo 'FTP server address:' $FTP_SERVER
+	echo 'FTP server port:' $FTP_SERVER_PORT
+	echo 'FTP server user:' $FTP_USERNAME
+	echo 'FTP server password:' $FTP_PASSWORD
+	echo 'Disk to backup:' $DISK_TARGET
+	echo 'Partition type:' $PARTITION_TYPE	
 	
-	cpt=0
+	CPT_INCREMENT=0
 
-	while [ $cpt -lt $count ]
+	while [ $CPT_INCREMENT -lt $TOTAL_PACKETS_NUMBER ]
 	do
 		#catpure block of block size predefined
-		dd if=$ddToBackup bs=$bs count=1 skip=$cpt of=$cpt$fileName
+		dd if=$DISK_TARGET bs=$BLOCK_SIZE count=1 skip=$CPT_INCREMENT of=$CPT_INCREMENT$BACKUP_FILENAME
 		
 		#send block to ftp server
-		ftpput -v -u $ftpUser -p $ftpPwd $ftpServer:$ftpServerPort $cpt$fileName
+		ftpput -v -u $FTP_USERNAME -p $FTP_PASSWORD $FTP_SERVER:$FTP_SERVER_PORT $BACKUP_PATH/$CPT_INCREMENT$BACKUP_FILENAME
 		
 		#delete block
-		rm $cpt$fileName
+		rm $CPT_INCREMENT$BACKUP_FILENAME
 		
 		#increment cpt
-		let 'cpt=cpt+1'
+		let 'CPT_INCREMENT=CPT_INCREMENT+1'
 		
 		#calcultate % to finished
-		let 'percent=cpt*100/count'
+		PROGRESS_PERCENT=$(($CPT_INCREMENT*100/$TOTAL_PACKETS_NUMBER))
 		
-		echo $percent'%to finished'
+		echo $PROGRESS_PERCENT'%to finished'
 	done
 
 	echo -e '\e[32mBackup Finished !\e[0m'
 else
 	echo "Configuration of restore image:"
-	echo 'Backup files name:' $fileName
-	echo 'Block size:' $bs
-	echo 'Number of block to restore:' $count
-	echo 'FTP server address:' $ftpServer
-	echo 'FTP server port:' $ftpServerPort
-	echo 'FTP server user:' $ftpUser
-	echo 'FTP server password:' $ftpPwd
-	echo 'Target disk to restore:' $ddToBackup
-	echo 'Partition type:' $partitionType
+	echo 'Backup files name:' $BACKUP_FILENAME
+	echo 'Block size:' $BLOCK_SIZE
+	echo 'Number of block to restore:' $TOTAL_PACKETS_NUMBER
+	echo 'FTP server address:' $FTP_SERVER
+	echo 'FTP server port:' $FTP_SERVER_PORT
+	echo 'FTP server user:' $FTP_USERNAME
+	echo 'FTP server password:' $FTP_PASSWORD
+	echo 'Target disk to restore:' $DISK_TARGET
+	echo 'Partition type:' $PARTITION_TYPE
 	
-	cpt=0
+	CPT_INCREMENT=0
 
-	while [ $cpt -lt $count ]
+	while [ $CPT_INCREMENT -lt $TOTAL_PACKETS_NUMBER ]
 	do
 		#download block from ftp
-		ftpget -v -u $ftpUser -p $ftpPwd $ftpServer:$ftpServerPort $cpt$fileName
+		ftpget -v -u $FTP_USERNAME -p $FTP_PASSWORD $FTP_SERVER:$FTP_SERVER_PORT $BACKUP_PATH/$CPT_INCREMENT$BACKUP_FILENAME
 	
 		#catpure block of block size predefined
-		dd if=$cpt$fileName bs=$bs count=1 seek=$cpt of=$ddToBackup
+		dd if=$CPT_INCREMENT$BACKUP_FILENAME bs=$BLOCK_SIZE count=1 seek=$CPT_INCREMENT of=$DISK_TARGET
 		
 		#delete block
-		rm $cpt$fileName
+		rm $CPT_INCREMENT$BACKUP_FILENAME
 		
 		#increment cpt
-		let 'cpt=cpt+1'
+		let 'CPT_INCREMENT=CPT_INCREMENT+1'
 		
 		#calcultate % to finished
-		let 'percent=cpt*100/count'
+		PROGRESS_PERCENT=$(($CPT_INCREMENT*100/$TOTAL_PACKETS_NUMBER))
 		
-		echo $percent'%to finished'
+		echo $PROGRESS_PERCENT'%to finished'
 	done
 	
 	echo -e '\e[32mRestore Finished !\e[0m'
